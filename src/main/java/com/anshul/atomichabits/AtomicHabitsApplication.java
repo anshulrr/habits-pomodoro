ffebf22017513e99e460dc7c270ca974d75432c7
anshul <ansh14j@gmail.com>
2023-01-18 13:57:07 +0530

    initial commit

---
+++ src/main/java/com/anshul/atomichabits/AtomicHabitsApplication.java
@@ -0,0 +1,13 @@
+package com.anshul.atomichabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicHabitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicHabitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anshul/atomichabits/AtomicHabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshul.atomichabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicHabitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
