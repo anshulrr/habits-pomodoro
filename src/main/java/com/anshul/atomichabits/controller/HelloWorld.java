c6a77649a0fe23b8902e0096c10b7bb1bb4c2a27
anshul <ansh14j@gmail.com>
2023-02-02 14:26:41 +0530

    hello world controller: understand the flow

---
+++ src/main/java/com/anshul/atomichabits/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.anshul.atomichabits.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+	@GetMapping("/")
+	public String getResponse(Authentication authentication) {
+		System.out.println(authentication.getPrincipal());
+		return "Hello " + authentication.getName();
+	}
+}
