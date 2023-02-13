1fbefc23707c1710310f9a577f7f6dd14c8c4b56
anshul <ansh14j@gmail.com>
2023-02-13 13:19:00 +0530

    configuration for CORS and OPTIONS requests

--- src/main/java/com/anshul/atomichabits/security/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/anshul/atomichabits/security/BasicAuthSecurityConfiguration.java
@@ -4,11 +4,14 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -17,6 +20,7 @@ public class BasicAuthSecurityConfiguration {
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		
 		http.authorizeHttpRequests()
+			.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 			.requestMatchers("/signup").permitAll()	// removing auth for signup
 			.anyRequest().authenticated();
 		
@@ -35,6 +39,17 @@ public class BasicAuthSecurityConfiguration {
 		return http.build();
 	}
 	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+
 	// required for default spring security Todo: why
 	@Bean
 	public UserDetailsService userDetailService(DataSource dataSource) {
