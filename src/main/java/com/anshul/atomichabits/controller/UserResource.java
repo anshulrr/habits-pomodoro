10de3ade1e594524f80616b417d08d4d1fdda602
anshul <ansh14j@gmail.com>
2023-07-16 08:00:10 +0530

    change password api

--- src/main/java/com/anshul/atomichabits/controller/UserResource.java
+++ src/main/java/com/anshul/atomichabits/controller/UserResource.java
@@ -1,19 +1,25 @@
 package com.anshul.atomichabits.controller;
 
 import java.net.URI;
+import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.anshul.atomichabits.jpa.UserRepository;
+import com.anshul.atomichabits.model.ProjectCategory;
 import com.anshul.atomichabits.model.User;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 
 @RestController
 public class UserResource {
@@ -37,4 +43,21 @@ public class UserResource {
 				.toUri();
 		return ResponseEntity.created(location).build();
 	}
+
+	@PutMapping("/users/change-password")
+	public ResponseEntity<User> createProjectOfUser(@Valid @RequestBody PasswordDto passwordDto,
+			Principal principal) {
+
+		System.out.println(passwordDto.password());
+		
+		Optional<User> user = userRepository.findByUsername(principal.getName());
+		
+		user.get().setPassword(passwordDto.password());
+		
+		userRepository.save(user.get());
+		
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
 }
+
+record PasswordDto(@NotBlank String password) {}
