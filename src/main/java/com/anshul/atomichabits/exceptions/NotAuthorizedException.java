51cb57e590e7fb0b3575df6f1f25946911eb2979
anshul <ansh14j@gmail.com>
2023-07-31 17:45:59 +0530

    cleanup

--- src/main/java/com/anshul/atomichabits/exceptions/NotAuthorizedException.java
+++ src/main/java/com/anshul/atomichabits/exceptions/NotAuthorizedException.java
@@ -3,8 +3,10 @@ package com.anshul.atomichabits.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
 public class NotAuthorizedException extends RuntimeException {
+
+	private static final long serialVersionUID = -5037535643782503981L;
 
 	public NotAuthorizedException(String message) {
 		super(message);
--- src/main/java/com/anshul/atomichabits/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/anshul/atomichabits/exceptions/ResourceNotFoundException.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 6602951767538627743L;
+
 	public ResourceNotFoundException(String message) {
 		super(message);
 	}
