688a9c6a5a26203f68583ea5c1f764098bb46bc0
anshul <ansh14j@gmail.com>
2023-08-04 15:17:11 +0530

    first test: WIP

---
+++ src/test/java/com/anshul/atomichabits/controller/HelloWorldTest.java
@@ -0,0 +1,29 @@
+package com.anshul.atomichabits.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.*;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@WebMvcTest(HelloWorld.class)
+class HelloWorldTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	void getHelloWorld_basic() throws Exception {
+		RequestBuilder request = MockMvcRequestBuilders.get("/")
+				.accept(MediaType.APPLICATION_JSON);
+
+		MvcResult result = mockMvc.perform(request)
+				.andReturn();
+
+		assertEquals("Hello", result.getResponse().getContentAsString());
+	}
+
+}
