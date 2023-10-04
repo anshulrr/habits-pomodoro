3050d21cd875e528771d1061ce680bcaf7107f2c
anshul <ansh14j@gmail.com>
2023-10-04 16:49:43 +0530

    cleanup

--- src/main/java/com/anshul/atomichabits/jpa/TaskRepository.java
+++ src/main/java/com/anshul/atomichabits/jpa/TaskRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.anshul.atomichabits.dto.TaskForList;
-import com.anshul.atomichabits.model.Tag;
 import com.anshul.atomichabits.model.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
