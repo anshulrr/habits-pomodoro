600dad75603fb6d265b5378f42cc6940b1dc53ea
anshul <ansh14j@gmail.com>
2024-01-19 11:56:08 +0530

    comment search query update to filter matching task comments

--- src/main/java/com/anshul/atomichabits/jpa/CommentRepository.java
+++ src/main/java/com/anshul/atomichabits/jpa/CommentRepository.java
@@ -55,13 +55,16 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 			""", nativeQuery = true)
 	public Integer getUserCommentsWithReviseDateCount(Long user_id, String status, long[] categoryIds);
 	
+	// TODO: optimize query for with 'or' for task description match
 	@Query(value = """
 			select c.*, c.created_at createdAt, c.revise_date reviseDate, pc.name category, p.name project, p.color color, t.description task  
 			from comments c
 			left join project_categories pc on c.project_category_id = pc.id
 			left join projects p on c.project_id = p.id
 			left join tasks t on c.task_id = t.id
-			where c.user_id = :user_id and c.status = :status and websearch_to_tsquery('english', :searchString)  @@ to_tsvector('english', c.description)
+			where c.user_id = :user_id and c.status = :status 
+			and (websearch_to_tsquery('english', :searchString)  @@ to_tsvector('english', c.description)
+				or websearch_to_tsquery('english', :searchString)  @@ to_tsvector('english', t.description))
 			and (c.project_category_id in :categoryIds or c.project_category_id is null)
 			order by c.id desc
 			limit :limit offset :offset
@@ -69,9 +72,12 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
 	public List<CommentForList> retrieveUserSearchedComments(Long user_id, String status, int limit, int offset, long[] categoryIds, String searchString);
 	
 	@Query(value = """
-			select count(*) from comments 
-			where user_id = :user_id and status = :status and websearch_to_tsquery('english', :searchString)  @@ to_tsvector('english', description)
-			and (project_category_id is null or project_category_id in :categoryIds) 
+			select count(*) from comments c
+			left join tasks t on c.task_id = t.id
+			where c.user_id = :user_id and c.status = :status 
+			and (websearch_to_tsquery('english', :searchString)  @@ to_tsvector('english', c.description)
+				or websearch_to_tsquery('english', :searchString)  @@ to_tsvector('english', t.description))
+			and (c.project_category_id is null or c.project_category_id in :categoryIds) 
 			""", nativeQuery = true)
 	public Integer getUserSearchedCommentsCount(Long user_id, String status, long[] categoryIds, String searchString);
 	
