1bc9a864bf759c01e07d6fee63f79516cf4cb7c9
anshul <ansh14j@gmail.com>
2024-01-23 18:30:08 +0530

    tests cleanup

--- src/test/java/com/anshul/atomichabits/business/ProjectCategoryServiceTest.java
+++ src/test/java/com/anshul/atomichabits/business/ProjectCategoryServiceTest.java
@@ -36,76 +36,87 @@ class ProjectCategoryServiceTest {
 	
 	@Mock
 	private ProjectCategoryRepository projectCategoryRepositoryMock;
+	
+	static Long USER_ID = 1L;
+	static Long CATEGORY_ID = 11L;
 
 	@Test
 	void retriveProjectCategory() {
-		when(projectCategoryRepositoryMock.findUserProjectCategoryById(1L, 2L))
+		when(projectCategoryRepositoryMock.findUserProjectCategoryById(USER_ID, CATEGORY_ID))
 			.thenReturn(Optional.of(new ProjectCategory()));
 		
-		ProjectCategory projectCategory = projectCategoryService.retriveProjectCategory(1L, 2L);
+		ProjectCategory projectCategory = projectCategoryService.retriveProjectCategory(USER_ID, CATEGORY_ID);
 		
 		assertNotNull(projectCategory);
 	}
 	
 	@Test
 	void retriveProjectCategoryEmpty() {
-		when(projectCategoryRepositoryMock.findUserProjectCategoryById(1L, 2L))
+		when(projectCategoryRepositoryMock.findUserProjectCategoryById(USER_ID, CATEGORY_ID))
 			.thenReturn(Optional.ofNullable(null));
 		
 		Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
-			projectCategoryService.retriveProjectCategory(1L, 2L);
+			projectCategoryService.retriveProjectCategory(USER_ID, CATEGORY_ID);
 	    });
-	    assertEquals("project category id:" + 2L, exception.getMessage());
+	    assertEquals("project category id:" + CATEGORY_ID, exception.getMessage());
 	}
 	
 	@Test
 	void retrieveAllProjectCategories() {
-		when(projectCategoryRepositoryMock.findUserProjectCategories(1L, 0, 0))
-			.thenReturn(new ArrayList<ProjectCategory>());
+		int limit = 5;
+		int offset = 0;
 		
-		List<ProjectCategory> categories = projectCategoryService.retrieveAllProjectCategories(1L, 0, 0);
+		List<ProjectCategory> categories = new ArrayList<>();
+		categories.add(new ProjectCategory());
 		
-		assertEquals(0, categories.size());
+		when(projectCategoryRepositoryMock.findUserProjectCategories(USER_ID, limit, offset))
+			.thenReturn(categories);
+		
+		List<ProjectCategory> retrievedCategories = projectCategoryService.retrieveAllProjectCategories(USER_ID, limit, offset);
+		
+		assertEquals(1, retrievedCategories.size());
 	}
 	
 	@Test
 	void retrieveAllProjectCategoriesCount() {
-		when(projectCategoryRepositoryMock.getUserProjectCategoriesCount(1L))
-			.thenReturn(0);
+		when(projectCategoryRepositoryMock.getUserProjectCategoriesCount(USER_ID))
+			.thenReturn(4);
 		
-		Integer categoriesCount = projectCategoryService.retrieveAllProjectCategoriesCount(1L);
+		Integer categoriesCount = projectCategoryService.retrieveAllProjectCategoriesCount(USER_ID);
 		
-		assertEquals(0, categoriesCount);
+		assertEquals(4, categoriesCount);
 	}
 	
 	@Test
 	void createProjectCategory() {
 		User user = new User("Samay", "samay@xyz.com");
 		
-		when(userRepositoryMock.findById(1L)).thenReturn(
+		when(userRepositoryMock.findById(USER_ID)).thenReturn(
 				Optional.of(user)
 				);
 		
 		ProjectCategory projectCategoryRequest = new ProjectCategory();
+		projectCategoryRequest.setColor("#afafaf");
 		
-		projectCategoryService.createProjectCategory(1L, projectCategoryRequest);
+		projectCategoryService.createProjectCategory(USER_ID, projectCategoryRequest);
 		
 		ArgumentCaptor<ProjectCategory> captor = ArgumentCaptor.forClass(ProjectCategory.class);
 		verify(projectCategoryRepositoryMock).save(captor.capture());
 		
 		assertEquals(user, captor.getValue().getUser());
+		assertEquals("#afafaf", captor.getValue().getColor());
 	}
 	
 	@Test
 	void updateProjectCategory() {
 		ProjectCategory projectCategory = new ProjectCategory();
-		when(projectCategoryRepositoryMock.findUserProjectCategoryById(1L, 2L))
+		when(projectCategoryRepositoryMock.findUserProjectCategoryById(USER_ID, CATEGORY_ID))
 			.thenReturn(Optional.of(projectCategory));
 		
 		ProjectCategory projectCategoryRequest = new ProjectCategory();
 		projectCategoryRequest.setLevel(0);
 		
-		projectCategoryService.updateProjectCategory(1L, 2L, projectCategoryRequest);
+		projectCategoryService.updateProjectCategory(USER_ID, CATEGORY_ID, projectCategoryRequest);
 		
 		ArgumentCaptor<ProjectCategory> captor = ArgumentCaptor.forClass(ProjectCategory.class);
 		verify(projectCategoryRepositoryMock).save(captor.capture());
@@ -116,14 +127,14 @@ class ProjectCategoryServiceTest {
 	
 	@Test
 	void updateProjectCategoryEmpty() {
-		when(projectCategoryRepositoryMock.findUserProjectCategoryById(1L, 2L))
+		when(projectCategoryRepositoryMock.findUserProjectCategoryById(USER_ID, CATEGORY_ID))
 			.thenReturn(Optional.ofNullable(null));
 		
 		ProjectCategory projectCategoryRequest = new ProjectCategory();
 		
 		Exception exception = assertThrows(ResourceNotFoundException.class, () -> {
-			projectCategoryService.updateProjectCategory(1L, 2L, projectCategoryRequest);
+			projectCategoryService.updateProjectCategory(USER_ID, CATEGORY_ID, projectCategoryRequest);
 	    });
-	    assertEquals("project category id:" + 2L, exception.getMessage());
+	    assertEquals("project category id:" + CATEGORY_ID, exception.getMessage());
 	}
 }
