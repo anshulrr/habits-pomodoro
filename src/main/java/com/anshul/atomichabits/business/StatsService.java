f46f354972b7bd87c2f2fc76ab559b664a883459
anshul <ansh14j@gmail.com>
2026-02-15 18:44:54 +0530

    fix for sorting by priority and levels

--- src/main/java/com/anshul/atomichabits/business/StatsService.java
+++ src/main/java/com/anshul/atomichabits/business/StatsService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.anshul.atomichabits.dto.IndexedTime;
 import com.anshul.atomichabits.dto.TotalChartProjectData;
+import com.anshul.atomichabits.dto.TotalChartTaskData;
 import com.anshul.atomichabits.jpa.PomodoroRepository;
 
 import lombok.AllArgsConstructor;
@@ -21,20 +22,24 @@ import lombok.extern.slf4j.Slf4j;
 public class StatsService {
 
 	private PomodoroRepository pomodoroRepository;
-	
-	public List<Object> retrieveProjectCategoriesPomodoros(Long userId, OffsetDateTime startDate, OffsetDateTime endDate, long[] categories) {
+
+	public List<Object> retrieveProjectCategoriesPomodoros(Long userId, OffsetDateTime startDate,
+			OffsetDateTime endDate, long[] categories) {
 		return pomodoroRepository.findProjectCategoriesTime(userId, startDate, endDate, categories);
 	}
-	
-	public List<Object> retrieveProjectPomodoros(Long userId, OffsetDateTime startDate, OffsetDateTime endDate, long[] categories) {
+
+	public List<Object> retrieveProjectPomodoros(Long userId, OffsetDateTime startDate, OffsetDateTime endDate,
+			long[] categories) {
 		return pomodoroRepository.findProjectsTime(userId, startDate, endDate, categories);
 	}
-	
-	public List<Object> retrieveTaskPomodoros(Long userId, OffsetDateTime startDate, OffsetDateTime endDate, long[] categories) {
+
+	public List<Object> retrieveTaskPomodoros(Long userId, OffsetDateTime startDate, OffsetDateTime endDate,
+			long[] categories) {
 		return pomodoroRepository.findTasksTime(userId, startDate, endDate, categories);
 	}
-	
-	public Map<String, TotalChartProjectData> retrieveTotalPomodoros(String entity, Long userId, String limit, OffsetDateTime startDate, OffsetDateTime endDate, long[] categories, String timezone) {
+
+	public Map<String, TotalChartProjectData> retrieveProjectsTotalPomodoros(Long userId, String limit,
+			OffsetDateTime startDate, OffsetDateTime endDate, long[] categories, String timezone) {
 		// temporary fix for timezone issue with latest postgres / IANA timezone
 		if (timezone.equals("Asia/Calcutta")) {
 			timezone = "Asia/Kolkata";
@@ -48,31 +53,67 @@ public class StatsService {
 		} else if (limit.equals("yearly")) {
 			limit = "YYYY";
 		}
-		
+
 		List<String[]> result;
-		if (entity.equals("task")) {
-			result = pomodoroRepository.findTasksTotalTime(userId, startDate, endDate, categories, timezone, limit);
-		} else {			
-			result = pomodoroRepository.findTotalTime(userId, startDate, endDate, categories, timezone, limit);
-		}
-		log.trace("total time result: {}", result);
+		result = pomodoroRepository.findTotalTime(userId, startDate, endDate, categories, timezone, limit);
 
 		Map<String, TotalChartProjectData> groupedResult = new HashMap<>();
-		
-		for (String[] strArr: result) {
+
+		for (String[] strArr : result) {
 			String key = strArr[2];
 			if (!groupedResult.containsKey(key)) {
 				List<IndexedTime> dataArr = new ArrayList<>();
-				TotalChartProjectData obj = new TotalChartProjectData(dataArr, strArr[3], strArr[4], Integer.valueOf(strArr[5]), Integer.valueOf(strArr[6]));
+				TotalChartProjectData obj = new TotalChartProjectData(dataArr, strArr[3], strArr[4],
+						Integer.valueOf(strArr[5]), Integer.valueOf(strArr[6]));
 				groupedResult.put(key, obj);
 			}
-			groupedResult.get(key).dataArr().add(new IndexedTime(Integer.valueOf(strArr[0]), Integer.valueOf(strArr[1])));
+			groupedResult.get(key).dataArr()
+					.add(new IndexedTime(Integer.valueOf(strArr[0]), Integer.valueOf(strArr[1])));
 		}
+		log.trace("total time result: {}", result);
 
 		return groupedResult;
 	}
-	
-	public List<Object> retrievePomodorosCount(Long userId, OffsetDateTime startDate, OffsetDateTime endDate, String type, Long typeId, long[] categories, String timezone) {
+
+	public Map<String, TotalChartTaskData> retrieveTasksTotalPomodoros(Long userId, String limit,
+			OffsetDateTime startDate, OffsetDateTime endDate, long[] categories, String timezone) {
+		// temporary fix for timezone issue with latest postgres / IANA timezone
+		if (timezone.equals("Asia/Calcutta")) {
+			timezone = "Asia/Kolkata";
+		}
+		if (limit.equals("daily")) {
+			limit = "DD";
+		} else if (limit.equals("weekly")) {
+			limit = "IW";
+		} else if (limit.equals("monthly")) {
+			limit = "MM";
+		} else if (limit.equals("yearly")) {
+			limit = "YYYY";
+		}
+
+		List<String[]> result;
+		result = pomodoroRepository.findTasksTotalTime(userId, startDate, endDate, categories, timezone, limit);
+
+		Map<String, TotalChartTaskData> groupedResult = new HashMap<>();
+
+		for (String[] strArr : result) {
+			String key = strArr[2];
+			if (!groupedResult.containsKey(key)) {
+				List<IndexedTime> dataArr = new ArrayList<>();
+				TotalChartTaskData obj = new TotalChartTaskData(dataArr, strArr[3], strArr[4],
+						Integer.valueOf(strArr[5]), Integer.valueOf(strArr[6]), Integer.valueOf(strArr[7]));
+				groupedResult.put(key, obj);
+			}
+			groupedResult.get(key).dataArr()
+					.add(new IndexedTime(Integer.valueOf(strArr[0]), Integer.valueOf(strArr[1])));
+		}
+		log.trace("total time result: {}", result);
+
+		return groupedResult;
+	}
+
+	public List<Object> retrievePomodorosCount(Long userId, OffsetDateTime startDate, OffsetDateTime endDate,
+			String type, Long typeId, long[] categories, String timezone) {
 		// temporary fix for timezone issue with latest postgres / IANA timezone
 		// TODO: find generic solution
 		if (timezone.equals("Asia/Calcutta")) {
@@ -90,7 +131,7 @@ public class StatsService {
 		}
 		return result;
 	}
-	
+
 	public List<Object> retrieveTaskPomodoros(Long userId, Long taskId, int limit, int offset) {
 		return pomodoroRepository.findTaskPomodoros(userId, taskId, limit, offset);
 	}
@@ -99,4 +140,3 @@ public class StatsService {
 		return pomodoroRepository.getTaskPomodorosCount(userId, taskId);
 	}
 }
-
