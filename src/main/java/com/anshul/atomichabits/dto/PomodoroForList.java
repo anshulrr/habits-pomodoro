17bd3076b1311c683407298d78120315bc2747e8
anshul <ansh14j@gmail.com>
2026-03-08 09:15:29 +0530

    pomodoros get relations

--- src/main/java/com/anshul/atomichabits/dto/PomodoroForList.java
+++ src/main/java/com/anshul/atomichabits/dto/PomodoroForList.java
@@ -26,5 +26,7 @@ public interface PomodoroForList {
 
 	Instant getUpdatedAt();
 	
-	String getProjectId();
+	Long getProjectId();
+	
+	Long getCategoryId();
 }
--- src/main/java/com/anshul/atomichabits/jpa/PomodoroRepository.java
+++ src/main/java/com/anshul/atomichabits/jpa/PomodoroRepository.java
@@ -28,7 +28,8 @@ public interface PomodoroRepository extends JpaRepository<Pomodoro, Long> {
 	public List<PomodoroDto> findRunningPomodoros(Long userId);
 
 	@Query("""
-			select p.id id, p.publicId publicId, p.status status, p.startTime startTime, p.endTime endTime, p.timeElapsed timeElapsed, p.task.id taskId, p.task.description task, p.task.project.color color, p.task.project.id projectId
+			select p.id id, p.publicId publicId, p.status status, p.startTime startTime, p.endTime endTime, p.timeElapsed timeElapsed, 
+			p.task.id taskId, p.task.description task, p.task.project.color color, p.task.project.id projectId, p.task.project.projectCategory.id categoryId
 			from pomodoros p
 			where p.user.id = :userId and p.endTime >= :start and p.endTime <= :end and p.status in ('completed', 'past') and p.task.project.projectCategory.id in (:categories)
 			order by p.endTime desc, p.id desc
@@ -37,7 +38,7 @@ public interface PomodoroRepository extends JpaRepository<Pomodoro, Long> {
 	
 	@Query("""
 			select p.id id, p.publicId publicId, p.status status, p.startTime startTime, p.endTime endTime, p.timeElapsed timeElapsed, p.updatedAt updatedAt,
-			p.task.id taskId, p.task.description task, p.task.project.color color, p.task.project.id projectId
+			p.task.id taskId, p.task.description task, p.task.project.color color, p.task.project.id projectId, p.task.project.projectCategory.id categoryId
 			from pomodoros p
 			where p.user.id = :userId and p.endTime >= :start and p.endTime <= :end and p.status in ('completed', 'past') and p.updatedAt > :lastSyncTime
 			order by p.endTime desc, p.id desc
