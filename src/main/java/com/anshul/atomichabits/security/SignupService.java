4592135bde2f09fa82f01468cc8292916af3d6b1
Anshul <ansh14j@gmail.com>
2026-05-01 06:36:08 +0530

    hot fix - signup with uuid

--- src/main/java/com/anshul/atomichabits/security/SignupService.java
+++ src/main/java/com/anshul/atomichabits/security/SignupService.java
@@ -1,5 +1,7 @@
 package com.anshul.atomichabits.security;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.anshul.atomichabits.jpa.*;
@@ -51,12 +53,14 @@ public class SignupService {
 
 	private void createInitialTags(User user) {
 		Tag tag1 = new Tag();
+		tag1.setId(UUID.randomUUID());
 		tag1.setName("daily");
 		tag1.setColor("#00c0f0");
 		tag1.setUser(user);
 		tagRepository.save(tag1);
 
 		Tag tag2 = new Tag();
+		tag2.setId(UUID.randomUUID());
 		tag2.setName("imp");
 		tag2.setColor("#046134");
 		tag2.setPriority(2);
@@ -67,6 +71,7 @@ public class SignupService {
 	private ProjectCategory createInitialProjectCategories(User user) {
 		// create two project category named General, Hobbies
 		ProjectCategory projectCategory1 = new ProjectCategory();
+		projectCategory1.setId(UUID.randomUUID());
 		projectCategory1.setUser(user);
 		projectCategory1.setName("General");
 		projectCategory1.setColor("#124f12");
@@ -75,6 +80,7 @@ public class SignupService {
 		ProjectCategory projectCategory = projectCategoryRepository.save(projectCategory1);
 
 		ProjectCategory projectCategory2 = new ProjectCategory();
+		projectCategory2.setId(UUID.randomUUID());
 		projectCategory2.setUser(user);
 		projectCategory2.setName("Health");
 		projectCategory2.setColor("#4a86e8");
@@ -83,6 +89,7 @@ public class SignupService {
 		projectCategoryRepository.save(projectCategory2);
 
 		ProjectCategory projectCategory3 = new ProjectCategory();
+		projectCategory3.setId(UUID.randomUUID());
 		projectCategory3.setUser(user);
 		projectCategory3.setName("Hobbies");
 		projectCategory3.setColor("#ff0000");
@@ -91,6 +98,7 @@ public class SignupService {
 		projectCategoryRepository.save(projectCategory3);
 
 		ProjectCategory projectCategory4 = new ProjectCategory();
+		projectCategory4.setId(UUID.randomUUID());
 		projectCategory4.setUser(user);
 		projectCategory4.setName("Rest");
 		projectCategory4.setColor("#919191");
@@ -104,6 +112,7 @@ public class SignupService {
 
 	private Project createInitialProject(User user, ProjectCategory projectCategory) {
 		Project project = new Project();
+		project.setId(UUID.randomUUID());
 		project.setUser(user);
 		project.setProjectCategory(projectCategory);
 		project.setName("General Project");
@@ -114,6 +123,7 @@ public class SignupService {
 
 	private Task createInitialTask(User user, Project project) {
 		Task task = new Task();
+		task.setId(UUID.randomUUID());
 		task.setUser(user);
 		task.setProject(project);
 		task.setDescription("Sample Task");
@@ -123,6 +133,7 @@ public class SignupService {
 
 	private void createInitialComments(User user, ProjectCategory projectCategory, Project project, Task task) {
 		Comment comment = new Comment();
+		comment.setId(UUID.randomUUID());
 		comment.setUser(user);
 		comment.setDescription("""
 				Sample note with some markdown syntax
@@ -133,12 +144,14 @@ public class SignupService {
 		commentRepository.save(comment);
 
 		Comment comment1 = new Comment();
+		comment1.setId(UUID.randomUUID());
 		comment1.setUser(user);
 		comment1.setProjectCategory(projectCategory);
 		comment1.setDescription("Sample note of a Project Category");
 		commentRepository.save(comment1);
 
 		Comment comment2 = new Comment();
+		comment2.setId(UUID.randomUUID());
 		comment2.setUser(user);
 		comment2.setProjectCategory(projectCategory);
 		comment2.setProject(project);
@@ -146,6 +159,7 @@ public class SignupService {
 		commentRepository.save(comment2);
 
 		Comment comment3 = new Comment();
+		comment3.setId(UUID.randomUUID());
 		comment3.setUser(user);
 		comment3.setProjectCategory(projectCategory);
 		comment3.setProject(project);
